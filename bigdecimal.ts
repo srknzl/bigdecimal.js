4e7658037e44bf1ba349a0fc954a165cac4329b4
Serkan Özel <srknzl.github@gmail.com>
2021-05-19 23:56:23 +0300

    Change license author

--- bigdecimal.ts
+++ bigdecimal.ts
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2021 Serkan Özel, Inc. All Rights Reserved.
+  Copyright (c) 2021 Serkan Özel. All Rights Reserved.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
