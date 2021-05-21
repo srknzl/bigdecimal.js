afb29034eb7dffb818a7976f3fa4a1dd64a134fc
Serkan Özel <srknzl.github@gmail.com>
2021-05-22 01:41:03 +0300

    update eslint disable this reference

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     ],
     'rules': {
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
