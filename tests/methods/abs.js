0419933c64e4fd60826e77fdc35587135b508e04
Serkan Özel <srknzl.github@gmail.com>
2021-06-21 11:39:17 +0300

    Fix typo

--- tests/methods/abs.js
+++ tests/methods/abs.js
@@ -6,14 +6,7 @@ chai.should();
 describe('Absolute value test', function () {
 
     it('should calculate abs correctly', function () {
-        for (const test of [{
-            "arguments": [
-                "999130309696269960369411189504977138022060325178635804383777964396643757881930599444544232699066835337985580790738",
-                1,
-                4
-            ],
-            "result": "1E+114"
-        }]) {
+        for (const test of testCases) {
             const absOp = () => {
                 return BigDecimal.fromValue(test.arguments[0]).abs(
                     new MathContext(test.arguments[1], test.arguments[2])
