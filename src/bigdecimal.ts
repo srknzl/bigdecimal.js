e6d2941999b9a5a180096f0e7132232c32344e4a
Serkan Özel <serkanozel380@gmail.com>
2021-07-06 19:29:13 +0300

    Fix bug

--- src/bigdecimal.ts
+++ src/bigdecimal.ts
@@ -10,7 +10,7 @@
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
+  See the License for the specific language governing permissions andg
   limitations under the License.
 */
 
@@ -3201,7 +3201,7 @@ export class BigDecimal {
         if (insertionPoint === 0) { /* Point goes right before intVal */
             return (signum < 0 ? '-0.' : '0.') + intString;
         } else if (insertionPoint > 0) { /* Point goes inside intVal */
-            buf = buf.slice(0, insertionPoint) + '.' + buf.slice(insertionPoint);
+            buf = intString.slice(0, insertionPoint) + '.' + intString.slice(insertionPoint);
             if (signum < 0)
                 buf = '-' + buf;
         } else { /* We must insert zeros between point and intVal */
