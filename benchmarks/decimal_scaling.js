2195feb2c03815690ef726aa17f07be82790f274
d-mon- <olivier@cryptact.com>
2022-07-05 16:31:33 +0900

    Use GWT Rounding mode in decimal_scaling

--- benchmarks/decimal_scaling.js
+++ benchmarks/decimal_scaling.js
@@ -1,14 +1,17 @@
 'use strict';
 const Benchmark = require('benchmark');
 const Decimal = require('decimal.js');
-const { RoundingMode, MC } = require('../lib/bigdecimal.js');
+const { RoundingMode } = require('../lib/bigdecimal.js');
 const { bigDecimals, bigDecimalsBigjs, bigDecimalsBigNumber, bigDecimalsDecimal, bigDecimalsGWT } = require('./test_numbers');
 const { attachEventsAndRun } = require('./utils.js');
+const { RoundingMode: RoundingModeGwt } = require('bigdecimal');
 
-const suite = new Benchmark.Suite('Decimal scale');
+const suite = new Benchmark.Suite('Decimal scaling');
 
 const scales = [10, 1, 0];
 const length = scales.length;
+
+const GWT_HALF_UP = RoundingModeGwt.HALF_UP();
 
 suite.add('Bigdecimal.js', function () {
     for (let i = 0; i < bigDecimals.length - 1; i++) {
@@ -28,7 +31,7 @@ suite.add('Bigdecimal.js', function () {
     }
 }).add('GWTBased', function () {
     for (let i = 0; i < bigDecimalsGWT.length - 1; i++) {
-        bigDecimalsGWT[i].setScale(scales[i%length], RoundingMode.HALF_UP);
+        bigDecimalsGWT[i].setScale(scales[i%length], GWT_HALF_UP);
     }
 });
 
